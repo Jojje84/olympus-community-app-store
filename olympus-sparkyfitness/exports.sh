4c6ce0b040aea8ca9b927edcd911ecf83b5cd99f
Jorge <68773604+Jojje84@users.noreply.github.com>
2026-09-20 16:52:50 +0200

    Add SparkyFitness generated secrets

---
+++ olympus-sparkyfitness/exports.sh
@@ -0,0 +1,4 @@
+export APP_OLYMPUS_SPARKYFITNESS_DB_PASSWORD="$(derive_entropy "${app_entropy_identifier}-postgres-password")"
+export APP_OLYMPUS_SPARKYFITNESS_APP_DB_PASSWORD="$(derive_entropy "${app_entropy_identifier}-app-db-password")"
+export APP_OLYMPUS_SPARKYFITNESS_API_ENCRYPTION_KEY="$(derive_entropy "${app_entropy_identifier}-api-encryption-key")"
+export APP_OLYMPUS_SPARKYFITNESS_AUTH_SECRET="$(derive_entropy "${app_entropy_identifier}-better-auth-secret")"
