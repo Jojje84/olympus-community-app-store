9399afa85dd2c7ec46a71f7e2871517c2fddc175
Jorge <68773604+Jojje84@users.noreply.github.com>
2026-09-22 11:49:13 +0200

    fix: prepare ForgeCore beta 2 for dedicated external HDD

--- olympus-forgecore/exports.sh
+++ olympus-forgecore/exports.sh
@@ -1,1 +1,1 @@
-export APP_OLYMPUS_FORGECORE_STORAGE_ROOT="${FORGECORE_STORAGE_ROOT:-/mnt/forgecore}"
+export APP_OLYMPUS_FORGECORE_STORAGE_ROOT="${FORGECORE_STORAGE_ROOT:-/home/umbrel/umbrel/external/HDD/ForgeCore}"
